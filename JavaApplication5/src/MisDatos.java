4c62fc933e607feb9a523175cd7c539235334b8a
Alejandro Burciaga Calzadillas <alejbur@gmail.com>
2025-02-13 12:10:52 -0600

    Cambio en ultimo print

--- JavaApplication5/src/MisDatos.java
+++ JavaApplication5/src/MisDatos.java
@@ -5,6 +5,6 @@ public class MisDatos{
         System.out.println("Alejandro Burciaga Calzadillas");
         System.out.println("Ingenieriía en Sistemas Computacionales");
         System.out.println("7mo semestre");
-        System.out.println("Hola");
+        System.out.println("Hoy es jueves");
     }
 }
